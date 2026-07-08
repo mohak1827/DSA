3c179e0140521b44c49fe483c8e7c88a22cd315a
mohak1827 <mohaktaneja1827vk@gmail.com>
2026-07-09 02:19:30 +0530

    Time: 3 ms (92.69%) | Memory: 45 MB (70.29%)

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -3,9 +3,8 @@ class Solution {
         int[] ans = new int[nums2.length];
         Arrays.fill(ans,-1);
         for(int i = 0;i<nums2.length;i++){
-            int a = nums2[i];
             for(int j = i+1;j<nums2.length;j++){
-                if(nums2[j]>a){
+                if(nums2[j]>nums2[i]){
                     ans[i]=nums2[j];
                     break;
                 }
