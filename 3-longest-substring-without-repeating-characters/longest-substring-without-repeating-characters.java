47337f7ac3ae63642b7b5523e4782bde7f7a5d00
mohak1827 <mohaktaneja1827vk@gmail.com>
2026-07-09 16:45:30 +0530

    Time: 6 ms (70.24%) | Memory: 46.4 MB (47.80%)

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -3,17 +3,18 @@ class Solution {
         Set<Character> set = new HashSet<>();
         int count = 0;
         int l = 0;
-
+        int n = s.length();
         for(int r = 0; r < s.length(); r++)
         {
-            while(set.contains(s.charAt(r)))
+            char ch = s.charAt(r);
+            while(set.contains(ch))
             {
                 set.remove(s.charAt(l));
                 l++;
             }
             
             count = Math.max(count, r-l+1);
-            set.add(s.charAt(r));
+            set.add(ch);
             
         }
         return count;
