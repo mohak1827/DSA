720e1d39dfae5f7438a1b54f6bac5c3b3f4d1b15
mohak1827 <mohaktaneja1827vk@gmail.com>
2026-07-10 01:32:40 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (72.00%)

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.java
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        for(int i = 1; i < nums.length; i++)
+        int n = nums.length;
+        for(int i = 1; i <n; i++)
         {
             nums[i] += nums[i-1];
         }
