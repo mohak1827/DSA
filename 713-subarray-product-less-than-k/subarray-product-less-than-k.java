d42c2ac5e4001638e9633dd38c5d4d4a8db8309e
mohak1827 <mohaktaneja1827vk@gmail.com>
2026-07-09 17:13:53 +0530

    Time: 3 ms (91.75%) | Memory: 48.1 MB (95.78%)

---
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int count = 0;
+        if(k == 0)
+        {
+            return 0;
+        }
+        int pro = 1;
+        int l = 0;
+        for(int r = 0; r < nums.length; r++)
+        {
+            pro *= nums[r];
+            while(pro >= k)
+            {
+                pro /= nums[l];
+                l++;
+            }
+            count += r - l + 1;
+        }
+        return count;
+    }
+}
