164ae239e73a8f1dec679e5e74e5b8b4a04bc529
mohak1827 <mohaktaneja1827vk@gmail.com>
2026-07-11 04:47:08 +0530

    Time: 1 ms (99.95%) | Memory: 69.5 MB (11.86%)

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,19 +1,18 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int n = nums.length;
+        // int n = nums.length;
         int sum = 0;
         int min = Integer.MAX_VALUE;
         int l = 0;
-        for(int r = 0; r < n; r++)
+        for(int r = 0; r < nums.length; r++)
         {
             sum += nums[r];
-                while(sum >= target)
-                {
-                    min = Math.min(min,r-l+1);
-                    sum -= nums[l];
-                    l++;
-                }
-            
+            while(sum >= target)
+            {
+                min = Math.min(min,r-l+1);
+                sum -= nums[l];
+                l++;
+            }
         }
         return min==Integer.MAX_VALUE?0:min;
     }
