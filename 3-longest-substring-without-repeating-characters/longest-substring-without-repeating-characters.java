2db17a5228c0e6270ec94b696c56bc623f1c743d
mohak1827 <mohaktaneja1827vk@gmail.com>
2026-08-05 02:38:17 +0530

    Time: 66 ms (10.85%) | Memory: 48.1 MB (6.35%)

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -7,16 +7,15 @@ class Solution {
         Set<Character> set = new HashSet<>();
         while(r < n)
         {
-            if(set.contains(s.charAt(r)))
+            while(set.contains(s.charAt(r)))
             {
                 set.remove(s.charAt(l));
                 l++;
             }
-            else{
                 set.add(s.charAt(r));
                 maxLen = Math.max(maxLen, r-l+1);
                 r++;
-            }
+            
         }
         return maxLen;
     }
