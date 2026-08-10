bdbe99b91cf0630e83e76cebb6bc8b888443b5dd
mohak1827 <mohaktaneja1827vk@gmail.com>
2026-08-11 03:53:08 +0530

    Time: 61 ms (7.90%) | Memory: 70.9 MB (11.82%)

---
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        int l = 0;
+        for(int r = 0; r < fruits.length; r++)
+        {
+            map.put(fruits[r], map.getOrDefault(fruits[r],0)+1);
+            while(map.size() > 2)
+            {
+                map.put(fruits[l],map.get(fruits[l])-1);
+                if(map.get(fruits[l]) == 0)
+                {
+                    map.remove(fruits[l]);
+                }
+                l++;
+            }
+            count = Math.max(count, r-l+1);
+        }
+        for(int n : map.keySet())
+        {
+            System.out.println(n + " " + map.get(n));
+        }
+        return count;
+    }
+}
