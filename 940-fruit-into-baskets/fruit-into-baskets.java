235e7e67ea5474e8a6cb24ca3e74f2db71967274
mohak1827 <mohaktaneja1827vk@gmail.com>
2026-08-11 04:03:32 +0530

    Time: 51 ms (85.96%) | Memory: 70.5 MB (43.03%)

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -8,10 +8,11 @@ class Solution {
             map.put(fruits[r], map.getOrDefault(fruits[r],0)+1);
             while(map.size() > 2)
             {
-                map.replace(fruits[l],map.get(fruits[l])-1);
-                if(map.get(fruits[l]) == 0)
+                int fruit = fruits[l];
+                map.replace(fruit,map.get(fruit)-1);
+                if(map.get(fruit) == 0)
                 {
-                    map.remove(fruits[l]);
+                    map.remove(fruit);
                 }
                 l++;
             }
