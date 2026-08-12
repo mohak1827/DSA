ae32d954b44e95e56a5b9d35a75514a4120ad54f
mohak1827 <mohaktaneja1827vk@gmail.com>
2026-08-13 00:47:41 +0530

    Time: 26 ms (26.87%) | Memory: 47.1 MB (20.87%)

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -18,7 +18,7 @@ class Solution {
                 maxLen = Math.max(maxLen, r-l+1);
             }
             else{
-                if(r-l+1-maxFreq > k)
+                while(r-l+1-maxFreq > k)
                 {
                     char c = s.charAt(l);
                     int freq = map.get(c)-1;
