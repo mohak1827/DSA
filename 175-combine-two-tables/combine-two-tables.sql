1f2467a2bef455acf28201360e0121a242b7653b
mohak1827 <mohaktaneja1827vk@gmail.com>
2026-08-13 18:42:55 +0530

    Time: 463 ms (37.39%) | Memory: 0B (100.00%)

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,8 +1,1 @@
-SELECT
-    Person.firstName,
-    Person.lastName,
-    Address.city,
-    Address.state
-FROM Person
-LEFT JOIN Address
-ON Person.personId = Address.personId;
+select firstName, lastName, city, state from Person p left join Address a on p.personId = a.personId;
