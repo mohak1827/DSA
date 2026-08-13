9a4d282b6a6f17985b96cec375a1347b92c85c51
mohak1827 <mohaktaneja1827vk@gmail.com>
2026-08-13 06:04:06 +0530

    Time: 2 ms (92.03%) | Memory: 47.8 MB (57.65%)

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,16 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        for(int i = 0; i < nums.length; i++){
-            int j = i;
-            if(nums[i] == 0){
-                while(j < nums.length && nums[j] == 0){
-                    j++;
-                }
-                if(j == nums.length){
-                    break;
-                }
-                nums[i] = nums[j];
-                nums[j] = 0;
+        int l = 0;
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(nums[i] != 0)
+            {
+                int temp = nums[l];
+                nums[l] = nums[i];
+                nums[i] = temp;
+                l++;
             }
         }
     }
