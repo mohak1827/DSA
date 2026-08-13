4c745963475bcd6893cdef17429450b9d7f53ee6
mohak1827 <mohaktaneja1827vk@gmail.com>
2026-08-13 18:43:21 +0530

    Time: 410 ms (79.07%) | Memory: 0B (100.00%)

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,1 +1,1 @@
-select firstName, lastName, a.city, a.state from Person p left join Address a on p.personId = a.personId;
+select p.firstName, p.lastName, a.city, a.state from Person p left join Address a on p.personId = a.personId;
