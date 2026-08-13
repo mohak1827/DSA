f25c4cb5492c32d2b0ccc4a962ae9c9fbe63c789
mohak1827 <mohaktaneja1827vk@gmail.com>
2026-08-13 18:42:38 +0530

    Time: 850 ms (5.00%) | Memory: 0B (100.00%)

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,2 +1,8 @@
-# Write your MySQL query statement below
-select firstName, lastName, city, state from Person p left join Address a on p.personId = a.personId;
+SELECT
+    Person.firstName,
+    Person.lastName,
+    Address.city,
+    Address.state
+FROM Person
+LEFT JOIN Address
+ON Person.personId = Address.personId;
