734f786b52943fcc18fcd59e2f0258cbc8c9467f
mohak1827 <mohaktaneja1827vk@gmail.com>
2026-08-15 05:06:44 +0530

    Time: 2 ms (70.88%) | Memory: 55.6 MB (60.14%)

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int ele = nums[0];
+        int count = 0;
+        for(int num : nums)
+        {
+            if(count == 0)
+            {
+                ele = num;
+            }
+            if(num == ele)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+        }
+        return ele;
+    }
+}
