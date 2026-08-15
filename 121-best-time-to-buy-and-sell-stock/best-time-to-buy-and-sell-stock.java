864511e808786889d884c91ca646a1886077dc11
mohak1827 <mohaktaneja1827vk@gmail.com>
2026-08-15 18:37:55 +0530

    Time: 2 ms (42.57%) | Memory: 94.7 MB (15.78%)

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,11 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int profit=0;
-        int buyvalue=Integer.MAX_VALUE;
-        for(int i = 0; i < prices.length; i++)
+        int cost = Integer.MAX_VALUE;
+        int profit = 0;
+        for(int n : prices)
         {
-            buyvalue = Math.min(buyvalue, prices[i]);
-            profit = Math.max(profit, prices[i]-buyvalue);
+            cost = Math.min(cost, n);
+            profit = Math.max(profit, n - cost);
         }
         return profit;
     }
