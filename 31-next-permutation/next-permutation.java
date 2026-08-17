1f5f0b293c02e1d7e8b8089489f0abc124c4afc9
mohak1827 <mohaktaneja1827vk@gmail.com>
2026-08-17 17:36:33 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (16.23%)

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,50 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int idx = -1;
+        for(int i = nums.length - 2; i >= 0; i--)
+        {
+            if(nums[i] < nums[i+1])
+            {
+                idx = i;
+                break;
+            }
+        }
+        int k = nums.length - 1;
+        int val = k;
+        if(idx != -1)
+        {
+            while(k > idx)
+            {
+                if(nums[k] <= nums[idx])
+                {
+                    val--;
+                    k--;
+                    continue;
+                }
+                // if(nums[k] <= nums[val])
+                // {
+                //     val = k;
+                // }
+                k--;
+            }
+            if(nums[val] > nums[idx])
+            {
+                int temp = nums[idx];
+                nums[idx] = nums[val];
+                nums[val] = temp;
+            }
+        }
+        int m = idx+1;
+        int n = nums.length - 1;
+
+        while(m <= n)
+        {
+            int t = nums[m];
+            nums[m] = nums[n];
+            nums[n] = t;
+
+            m++;
+            n--;
+        }
+    }
+}
