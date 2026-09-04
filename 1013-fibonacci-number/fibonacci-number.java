1eff63a05cf7f5613539d389a2cbbcc5d28a531a
mohak1827 <mohaktaneja1827vk@gmail.com>
2026-09-04 18:49:14 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (70.37%)

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static int dp[] = new int[31];
+    static
+    {
+        Arrays.fill(dp,-1);
+    }
+    public int fib(int n) {
+        if(n <= 1) return n;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = fib(n-1)+fib(n-2);
+    }
+}
