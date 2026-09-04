2efa62ba39bfd96559e114d16febf8c6378aae26
mohak1827 <mohaktaneja1827vk@gmail.com>
2026-09-05 04:13:19 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (56.52%)

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,13 +1,24 @@
 class Solution {
     public int rob(int[] nums) {
-        int[] dp = new int[nums.length+1];
-        Arrays.fill(dp, -1);
-        return helper(nums.length-1,nums,dp);
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        for(int i = 1; i < nums.length; i++)
+        {
+            int l = Integer.MIN_VALUE;
+            if(i > 1)
+            {
+                l = nums[i] + dp[i-2];
+            }else l = nums[i];
+            int r = 0 + dp[i-1];
+            dp[i] = Math.max(l,r);
+        }
+        return dp[nums.length-1];
     }
     public int helper(int n, int[] nums, int[] dp)
     {
         if(n == 0) return nums[n];
         if(n < 0) return 0;
+
         if(dp[n] != -1) return dp[n];
 
         int l = nums[n] + helper(n-2, nums, dp);
