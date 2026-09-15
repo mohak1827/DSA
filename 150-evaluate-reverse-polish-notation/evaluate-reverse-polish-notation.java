80c1d3ac258f1dd97b5e0a327b3976009eedb100
mohak1827 <mohaktaneja1827vk@gmail.com>
2026-09-15 05:34:24 +0530

    Time: 8 ms (10.45%) | Memory: 47.1 MB (5.61%)

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,44 +1,44 @@
 class Solution {
     public int evalRPN(String[] tokens) {
         Stack<String> st = new Stack<>();
-        for(int i = 0; i < tokens.length; i++)
-        {
-            if(tokens[i].length() == 1 && !Character.isDigit(tokens[i].charAt(0)))
-            {
-                char ch = tokens[i].charAt(0);
-                if(ch == '+' || ch == '-' || ch == '/' || ch == '*')
-                {
-                    int p1 = Integer.parseInt(st.pop());
-                    int p2 = Integer.parseInt(st.pop());
 
-                    switch(ch)
-                    {
-                        case '+' :
-                            int res = p1+p2;
-                            st.push(String.valueOf(res));
-                            break;
-                        case '-' :
-                            res = p2-p1;
-                            st.push(String.valueOf(res));
-                            break;
-                        case '*' :
-                            res = p1*p2;
-                            st.push(String.valueOf(res));
-                            break;
-                        case '/' :
-                            res = p2/p1;
-                            st.push(String.valueOf(res));
-                            break;
-                        default :
-                            break;
-                    }
+        for(int i = 0; i < tokens.length; i++) {
+
+            if(tokens[i].equals("+") || 
+               tokens[i].equals("-") || 
+               tokens[i].equals("/") || 
+               tokens[i].equals("*")) {
+
+                int p1 = Integer.parseInt(st.pop());
+                int p2 = Integer.parseInt(st.pop());
+
+                int res = 0;
+
+                switch(tokens[i]) {
+                    case "+":
+                        res = p2 + p1;
+                        break;
+
+                    case "-":
+                        res = p2 - p1;
+                        break;
+
+                    case "*":
+                        res = p2 * p1;
+                        break;
+
+                    case "/":
+                        res = p2 / p1;
+                        break;
                 }
+
+                st.push(String.valueOf(res));
             }
-            else
-            {
+            else {
                 st.push(tokens[i]);
             }
         }
+
         return Integer.parseInt(st.peek());
     }
 }
