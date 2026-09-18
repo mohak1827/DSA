566dcfb86f511e12f101c532d51106ce88538c89
mohak1827 <mohaktaneja1827vk@gmail.com>
2026-09-18 22:01:18 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (16.44%)

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = i+1; j < n; j++)
+            {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+        for(int i = 0; i < n; i++)
+        {
+            int k = 0;
+            int l = n-1;
+            while(k <= l)
+            {
+                int tmp = matrix[i][k];
+                matrix[i][k] = matrix[i][l];
+                matrix[i][l] = tmp;
+
+                k++;
+                l--;
+            }
+        }
+    }
+}
