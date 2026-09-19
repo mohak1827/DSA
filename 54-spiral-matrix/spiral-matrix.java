7e6c6a00155d14e447f8b06856488ebafd60f34d
mohak1827 <mohaktaneja1827vk@gmail.com>
2026-09-19 06:23:43 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (62.45%)

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,6 +1,7 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> list = new ArrayList<>();
+        if(matrix.length == 0) return list;
         int top = 0;
         int bottom = matrix.length-1;
         int left = 0;
